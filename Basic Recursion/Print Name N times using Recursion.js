b7e4408f41a8e8e67f12fd4526467e5c09ec7df0
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-03-18 20:26:12 +0530

    recursion basic problems

---
+++ Basic Recursion/Print Name N times using Recursion.js
@@ -0,0 +1,16 @@
+// Print Name N times using Recursion
+
+// Problem Description: Given an integer N, write a program to print your name N times.
+
+class Solution {
+    printName(name, count, N) {
+        if (count === N) return; // Base case
+        console.log(name); // execution
+       this.printName(name, count+1, N); // Recursive call 
+    }
+}
+
+const sol =  new Solution();
+const N = 5;
+const name = "Suprakash";
+sol.printName(name,0,N);
